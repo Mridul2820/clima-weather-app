6ccecf4c889fa39d13073ad3865bcb40db34a905
Mridul2820 <immridulpanda2820@gmail.com>
2022-02-09 13:08:48 +0530

    base files copied

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:clima/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Text('Hello'),
+      home: LoadingScreen(),
     );
   }
 }
